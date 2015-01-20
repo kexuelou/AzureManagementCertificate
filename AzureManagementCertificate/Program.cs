3c5c8b87fbb9105363c50b522ee4e2304ec0b752
wzhao <cts-wzhao@live.com>
2015-01-20 14:24:54 +0800

    Given a Azure subscription profile, install it to the local certificate store. Later, can export as PFX file.

---
+++ AzureManagementCertificate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementCertificate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("AzureManagementCertificate -pf YourPublishSettingsFile");
+                Console.WriteLine(@"To download your Publish Setting File, visit https://windows.azure.com/download/publishprofile.aspx");
+                return;
+            }
+            string strCert = "";
+            bool valid = false;
+            using (StreamReader sr = new StreamReader(args[0])) 
+            {
+                while (sr.Peek() >= 0) 
+                {
+                    strCert = sr.ReadLine();
+                    if (strCert.TrimStart().StartsWith("ManagementCertificate"))
+                    {
+                        valid = true;
+                        break;
+                    }
+
+                }
+            }
+
+            if (!valid)
+                return;
+
+            strCert = strCert.Substring(strCert.IndexOf('"')+1);
+            strCert = strCert.Substring(0, strCert.IndexOf('"'));
+
+            X509Certificate2 azureCert= new X509Certificate2(Convert.FromBase64String(strCert), "", X509KeyStorageFlags.Exportable);
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            if (!store.Certificates.Contains(azureCert))
+            {
+                store.Add(azureCert);
+            }
+
+        } 
+
+    }
+}
